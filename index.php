eaa8e7bd609e7513959e7477a91f008cb6c6086d
Anthony Mochel <tony.mochel@gmail.com>
2017-12-09 14:39:35 +0100

    view login

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <?php require('./view/head.php'); ?>
+  <body>
+    <?php require('./view/header.php'); ?>
+    <?php require('./view/login.php'); ?>
+    <?php require('./view/script.php'); ?>
+  </body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,27 @@
+<header>
+  <div class="header_content">
+    <div class="header_brand">
+      <div class="row">
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+              <div class="header_img">
+                <img src="./asset/img/logo_suaps.png" alt="logo Suaps" />
+              </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-8 col-lg-9 col-xl-9">
+            <div class="header_title">
+              <h1>Réservations golf de la wantzenau</h1>
+              <p>Année 2017 - 2018</p>
+            </div>
+
+          </div>
+      </div>
+    </div>
+    <div class="header_nav">
+      <nav class="nav">
+        <a class="nav-link active" href="#">Connexion</a>
+        <a class="nav-link" href="#">Association AGJSEP</a>
+        <a class="nav-link" href="#">Inscription</a>
+      </nav>
+    </div>
+  </div>
+</header>
