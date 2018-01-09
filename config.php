6eaabe42b93fa03cdc6a7cc42beaf95686da1098
cptladouille <remymouttet67@gmail.com>
2018-01-09 08:16:50 +0100

    trehspioj

--- config.php
+++ config.php
@@ -12,7 +12,4 @@ define ('DB_PASSWORD','');					// mots de passe de connexion
 /* Gestion des liens dans le projet*/
 define ('ROOT_FOLDER', __DIR__);           // Chemin du dossier courrant
 define ('DS',DIRECTORY_SEPARATOR);         // Separation utiliser (linux /, windows \
-<<<<<<< HEAD
 ?>
-=======
->>>>>>> branch 'master' of https://gitlab.com/TonyMochel/Projet_SUAPS.git
